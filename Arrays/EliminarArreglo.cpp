d8546f7a0dd3ab6e90df3b181806c7498f070689
danimsix <dani.morales.sixto@gmail.com>
2022-08-24 11:05:14 -0500

    I add a new variable

--- Arrays/EliminarArreglo.cpp
+++ Arrays/EliminarArreglo.cpp
@@ -1,74 +1,72 @@
-
-
 #include <iostream>
 using namespace std;
 
-int main(void){
+int main(void)
+{
 
 	int numElementos;
-	
-	cout<<"Dígite el número de elementos a ingresar"<<endl;
-	cin>>numElementos;
-	
+
+	cout << "Dï¿½gite el nï¿½mero de elementos a ingresar" << endl;
+	cin >> numElementos;
+
 	int arreglo[numElementos];
 	int elemento;
-	
-	for (int i= 0; i<numElementos; i++){
-		cout<<"Ingrese el elemento número "<<i<<endl;
-		cin>>elemento;
+
+	for (int i = 0; i < numElementos; i++)
+	{
+		cout << "Ingrese el elemento nï¿½mero " << i << endl;
+		cin >> elemento;
 		arreglo[i] = elemento;
-		
 	}
-	
-	cout<<"Los elementos que ingreso fueron los siguientes"<<endl;
-	
-	
-	for (int i = 0; i<numElementos; i++){
-		
-		cout<<arreglo[i]<<" | ";
-		
+
+	cout << "Los elementos que ingreso fueron los siguientes" << endl;
+
+	for (int i = 0; i < numElementos; i++)
+	{
+
+		cout << arreglo[i] << " | ";
 	}
-	
-	cout<<endl;
-	cout<<"--------------------------------------------------------------"<<endl;
-	cout<<endl;
-	
+
+	cout << endl;
+	cout << "--------------------------------------------------------------" << endl;
+	cout << endl;
+
 	int elementoEliminar;
-	cout<<"Dígite el elemento a eliminar"<<endl;
-	cin>>elementoEliminar;
-	
+	cout << "Dï¿½gite el elemento a eliminar" << endl;
+	cin >> elementoEliminar;
+
 	int indiceAux = 0;
-	
-	while ((indiceAux<=numElementos) && (elementoEliminar != arreglo[indiceAux])){
-        indiceAux++;
-    }
 
-
-    if (indiceAux > numElementos){
-        cout << "No se encontro el elemento buscado"<<endl;
-    } else {
-    	for (int i = indiceAux; i<numElementos-1; i++){
-    		
-    		arreglo[i] = arreglo[i+1];
-    		
-		}
-		numElementos = numElementos-1;
+	while ((indiceAux <= numElementos) && (elementoEliminar != arreglo[indiceAux]))
+	{
+		indiceAux++;
 	}
-		
-		
 
-	
-	cout<<endl;
-	cout<<"--------------------------------------------------------------"<<endl;
-	cout<<endl;
-	
-	for (int i = 0; i<numElementos; i++){
-		
-		cout<<arreglo[i]<<" | ";
-		
-	}	
-	cout<<endl;
-	cout<<"--------------------------------------------------------------"<<endl;
-	cout<<endl;
+	if (indiceAux > numElementos)
+	{
+		cout << "No se encontro el elemento buscado" << endl;
+	}
+	else
+	{
+		for (int i = indiceAux; i < numElementos - 1; i++)
+		{
+
+			arreglo[i] = arreglo[i + 1];
+		}
+		numElementos = numElementos - 1;
+	}
+
+	cout << endl;
+	cout << "--------------------------------------------------------------" << endl;
+	cout << endl;
+
+	for (int i = 0; i < numElementos; i++)
+	{
+
+		cout << arreglo[i] << " | ";
+	}
+
+	cout << endl;
+	cout << "--------------------------------------------------------------" << endl;
+	cout << endl;
 }
-
