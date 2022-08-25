06dca0104b1d3867900372e494cd5156c53e483e
danimsix <dani.morales.sixto@gmail.com>
2022-08-25 11:04:40 -0500

    delete file

---
+++ Arrays/Array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int array[5] = {1, 2, 3, 4, 5};
+    int arrayReverse[5];
+
+    aux = 0;
+    int element;
+
+    for (int indexReverse = indexNormal; indexReverse > aux; indexReverse--)
+    {
+
+        arrayReverse[indexReverse] = array
+    }
+}
